d5a2ab0fed9ab82f027106b7fe9d0ec1e94f1752
ethereal-yun <2337831913@qq.com>
2022-06-14 21:13:17 +0800

    '上传了good页面部分代码'

--- App.vue
+++ App.vue
@@ -15,6 +15,7 @@
 
 <style lang="scss">
 	/*每个页面公共css */
+	@import "@/static/iconfont.css";
 	@import '@/uni_modules/uni-scss/index.scss';
 	/* #ifndef APP-NVUE */
 	@import '@/static/customicons.css';
