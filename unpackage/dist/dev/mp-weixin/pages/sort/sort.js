6e511a0cb13c1ee69d9af7ec47f31e4d892a3f4a
niuniu5 <mn15062116527@163.com>
2022-06-15 09:13:31 +0800

    xxx

--- unpackage/dist/dev/mp-weixin/pages/sort/sort.js
+++ unpackage/dist/dev/mp-weixin/pages/sort/sort.js
@@ -9,5 +9,5 @@ const _sfc_main = {
 function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
   return {};
 }
-var MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "D:/HBuilderXProject/ZLHome/pages/sort/sort.vue"]]);
+var MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "C:/Study/geek/15.uni-app/ZLHome/pages/sort/sort.vue"]]);
 wx.createPage(MiniProgramPage);
